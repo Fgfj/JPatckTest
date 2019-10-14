805df8b0cf9be294f0ae5390b9181342a077d95a
13683498647@163.com <123456yu>
2019-10-14 14:14:08 +0800

    111

---
+++ libtest/src/main/java/com/yz/libtest/jTestBean.java
@@ -0,0 +1,13 @@
+package com.yz.libtest;
+
+import android.util.Log;
+
+/**
+ * author:yz
+ * data: 2019/10/14,14:12
+ */
+public class jTestBean {
+    public static void ss(){
+        Log.i("Jtest","asdas");
+    }
+}
