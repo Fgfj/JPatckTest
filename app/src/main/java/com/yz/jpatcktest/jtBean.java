5e13fbbb9c077e3b3dcb55c5af5048220ae16f2e
13683498647@163.com <123456yu>
2019-10-14 15:11:36 +0800

    111

---
+++ app/src/main/java/com/yz/jpatcktest/jtBean.java
@@ -0,0 +1,13 @@
+package com.yz.jpatcktest;
+
+import android.util.Log;
+
+/**
+ * author:yz
+ * data: 2019/10/14,15:10
+ */
+public class jtBean  {
+    public static void toLog(){
+        Log.i("iii","sad");
+    }
+}
